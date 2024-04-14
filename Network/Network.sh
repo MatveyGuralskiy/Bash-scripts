6212127deae5ca06ad40a257014c95d31989c359
Matvey Guralskiy <mathewguralskiy@gmail.com>
2024-04-14 15:10:50 +0300

    Fix the IP variable

--- Network/Network.sh
+++ Network/Network.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP=$(ip addr show | grep inet | tail -n 1 | cut -d" " -f6 | cut -d"/" -f1)
+IP=$(ip addr show | grep "inet " | tail -n 1 | cut -d" " -f6 | cut -d"/" -f1)
 SUBNET_MASK=$(ip addr show | grep inet | tail -n 1 | cut -d" " -f6 | cut -d"/" -f2)
 GATEWAY=$(ip route show | head -n 1 | cut -d" " -f3)
 DNS=$(resolvectl | grep 'Current DNS' | cut -d" " -f4)
